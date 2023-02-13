5341e623e5ae43e4f5506989bb1264c36285c8a0
Pathakkriishna <kriishna.pathak@gmail.com>
2023-02-13 18:51:37 +0545

    assignment 1

---
+++ Krishna.java
@@ -0,0 +1,12 @@
+public class Krishna{
+    public static void main(String[] args){
+        System.out.println("Name of the Organization: Pathak Airlines\n" +"\n" +
+                         "Address:\n" +
+                         "----State: Bagmati\n" +
+                         "----Zip Code: 44600\n" +
+                         "----Country: Nepal\n" +"\n" +
+                         "Contact Information:\n" +
+                         "----Mobilen number: 9818821027\n" +
+                         "----Email: pathakairlines@gmail.com");
+    }
+}
